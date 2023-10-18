4a9d4285f03deddcaaf33d8694383cce330037a5
MossBrandCo <148348881+MossBrandCo@users.noreply.github.com>
2023-10-18 11:45:46 -0400

    First chatper added

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -6,7 +6,10 @@
 import Foundation
 
 func chapterOne() {
-    // Your portion of the story goes here
-    print("first push")
+    print("Everyday a kid named Jimmy dreams of getting back at the bullies who bully him everyday and has been trying to figure out how he can accomplish it. Then one day he realized that there is one day of the year that would be the best time for him to do it Halloween. Because its Halloween he realized that he could dress up as a known halloween killer and have the best chance to get away with getting them back. Ever since then he has been plotting and making a plan for what he will do to score his first victim on halloween.")
+        
+    //
+         print("first push")
+    
     print("blake branch")
 }
